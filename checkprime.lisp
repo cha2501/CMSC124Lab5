5a1a125bc5023c9ed1c3e01a9cf00a7f5abbc813
cha2501 <96985594+cha2501@users.noreply.github.com>
2022-11-15 14:37:43 +0800

    Update checkprime.lisp

--- checkprime.lisp
+++ checkprime.lisp
@@ -6,23 +6,23 @@
     (format t "~%Prime Numbers from 1 to ~a: ~%" n) 
     (terpri)
 
-    (loop 
-        (loop
-            (setq x (rem n k))
-            (setq k (+ k 1))
-            (when (or (= k n) (= x 0)) (return x))
+    (loop                                               ;first loop is the number to be divided or the dividend
+        (loop                                           ;second loop increments value 2 until n and serves as the operation's divisor
+            (setq x (rem n k))                          ; checks remainder of the operation (n rem k = rem)
+            (setq k (+ k 1))                            ;increments the divisor until such either one of when conditions is met and returns x
+            (when (or (= k n) (= x 0)) (return x))      ;if rem is 0, n is automatically evaluated as a composite number
         )
-        (if (> x 0)
+        (if (> x 0)                 ;prime number when returned x is more than 0
             (format t " ~a ~%" n)
-            (setq a (- a 1))
+            (setq a (- a 1))        ;prints prime number and updates counter
         )
-        (setq n (- n 1))
-        (setq k 2)
+        (setq n (- n 1))            ;decrements given n by 1 until when condition is met
+        (setq k 2)                  ;sets all necesarry variables back to original value
         (setq x 0)
         (setq h n)
-        (when (= n 1)
+        (when (= n 1)               ;1 has been reached; thus, evaluation breaks
             (terpri)
-            (format t "Total Number of Prime Numbers: ")(return a))
+            (format t "Total Number of Prime Numbers: ")(return a))     ;prints counter
     )
 )
 
